20a4bef488f8a2643cb0c3d5349d5c2f68876d16
Rohan Nayak <rohannayak2071@gmail.com>
2022-02-13 00:21:49 -0500

    setted up routes for users,auth,profile and posts

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ connectDB()
 
 app.get('/',(req,res) => res.send('API Running'))
 
+//Define Routes
+app.use('/api/users',require('./routes/api/users'))
+app.use('/api/auth',require('./routes/api/auth'))
+app.use('/api/profile',require('./routes/api/profile'))
+app.use('/api/posts',require('./routes/api/posts'))
+
 const PORT = process.env.ROOT || 6000;
 
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
