a297ac64c698f446a64c89f0156cd2941b74a1bd
Rohan Nayak <rohannayak2071@gmail.com>
2022-02-14 07:12:33 -0500

    added JWTToken Auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const normalize = require('normalize-url')
 const {check , validationResult} = require('express-validator/check')
 const User = require('../../models/User')
 
@@ -42,9 +45,15 @@ router.post('/',[
         const salt = await bcrypt.genSalt(10); // Hashing
         user.password = await bcrypt.hash(password,salt)
         await user.save()
-        // Return JSONWebToken JWT
-
-        res.send('User Registered')
+        const payload = {
+            user:{
+                id:user.id
+            }
+        }
+        jwt.sign(payload,config.get('jwtSecret'),{expiresIn:36000},()=>{
+            if(err) throw err;
+            res.json({token})
+        })
     }catch(err){
         console.error(err.message)
         res.status(500).send('Server error')
