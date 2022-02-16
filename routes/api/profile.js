56176e3598f4e6218fdfd87b21c9bfeb479d85c8
Rohan Nayak <rohannayak2071@gmail.com>
2022-02-16 16:54:51 -0500

    added code for getting all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -121,10 +121,31 @@ async (req,res)=>{
 
 router.get('/',async(req,res)=>{
     try {
-        const profile = await Profile.find().populate('user',['name','avatar'])
+        const profiles = await Profile.find().populate('user',['name','avatar'])
+        res.json(profiles)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+
+// @route GET api/profile/user/:user_id
+// @desc Getting Profiles by user id
+// @access Public
+
+router.get('/user/:user_id',async(req,res)=>{
+    try {
+        const profile = await Profile.findOne({ user:req.params.user_id }).populate('user',['name','avatar'])
+        if(!profile){
+            return res.status(400).json({ msg:' There is no profile for this user' })
+        }
         res.json(profile)
     } catch (err) {
         console.error(err.message)
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({ msg:'Profile not found' })
+        }
         res.status(500).send('Server Error')
     }
 })
