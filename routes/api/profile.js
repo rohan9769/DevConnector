35f6547619fe6d2332ba0bc771b2f7b870b80476
Rohan Nayak <rohannayak2071@gmail.com>
2022-02-15 19:56:45 -0500

    added profile validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
+const {check , validationResult} = require('express-validator/check')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
+
 // @route GET api/profile/me
 // @desc Get current users profile
 // @access Public
@@ -21,6 +23,36 @@ router.get('/me',auth,async(req,res)=>{
         console.error(err.message)
         res.status(500).send('Server Error')
     }
+})
+
+// @route POST api/profile
+// @desc Create or update user profile
+// @access Private
+router.post('/',[auth,
+    check('status','Status is required').not().isEmpty(),
+    check('skills','Skills is required').not().isEmpty()
+],
+async (req,res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors:errors.array() })
+    }
+    const{
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body;
+
+    
 })
 
 module.exports = router
