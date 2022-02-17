b53f2d487eee9da57debefc46cc2fa5b900cecfc
Rohan Nayak <rohannayak2071@gmail.com>
2022-02-17 14:40:37 -0500

    added routes for getting and deleting posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,4 +35,61 @@ router.post('/',[auth,[
     
 })
 
+
+// @route GET api/posts
+// @desc Gets all Posts
+// @access Private
+router.get('/',auth,async(req,res)=>{
+    try {
+        const posts = await Post.find().sort({ date:-1 })
+        res.json(posts)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route GET api/posts/:id
+// @desc Gets Post by id
+// @access Private
+router.get('/:id',auth,async(req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post){
+            return res.status(404).json({msg:'Post Not Found'})
+        }
+        res.json(post)
+    } catch (err) {
+        console.error(err.message)
+        if(err.kind == 'ObjectId'){
+            return res.status(404).json({msg:'Post Not Found'})
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route GET api/posts/:id
+// @desc Delete a post
+// @access Private
+router.delete('/:id',auth,async(req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id)
+
+        if(!post){
+            return res.status(404).json({msg:'Post Not Found'})
+        }
+        //Checking if the post belongs to a particular user
+        if(post.user !== req.user.id){
+            return res.status(401).json({msg:'User Not Authorized'})
+        }
+        await post.remove()
+        res.json({msg:'Post Removed'})
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+
+
 module.exports = router
