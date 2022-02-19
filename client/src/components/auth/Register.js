15df485088974298068baf59bfa889d5eeccddce
Rohan Nayak <rohannayak2071@gmail.com>
2022-02-19 11:23:33 -0500

    register user and getting token done

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,5 @@
-import { set } from "mongoose";
 import React,{useState} from "react";
-
+import axios from 'axios'
 const Register = () => {
     const [formData,setFormData] = useState({
         name:'',
@@ -10,13 +9,31 @@ const Register = () => {
     })
     const {name,email,password,password2} = formData
     const onChange = e => setFormData({...formData,[e.target.name]:e.target.value})
-    const onSubmit = e =>{
+    const onSubmit = async e =>{
         e.preventDefault()
         if(password != password2){
             console.log('Passwords do not match')
         }
         else{
-            console.log(formData)
+            const newUser = {
+                name,
+                email,
+                password,
+                password2
+            }
+            try{
+                const config = {
+                    headers:{
+                        'Content-Type':'application/json'
+                    }
+                }
+                const body = JSON.stringify(newUser)
+                const res = await axios.post('/api/users',body,config)
+                console.log(res.data)
+            }
+            catch(err){
+                console.error(err.response.data)
+            }
         }
     }
   return (
