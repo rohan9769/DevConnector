879f24e3465d90d6e2f7142b52abf92ebbbe5129
Rohan Nayak <rohannayak2071@gmail.com>
2022-02-19 17:24:09 -0500

    setted up alert reducer

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,12 +1,12 @@
-import {SET_ALERT,REMOVE_ALERT} from '../actions/types'
+import { SET_ALERT,REMOVE_ALERT } from '../actions/types'
 const initialState = [
     
 ]
 
-export default function(state=initialState,action){
+export default function (state=initialState,action){
     const {type,payload} = action
     switch(type){
-        case 'SET ALERT':
+        case 'SET_ALERT':
             return [...state,payload]
         case 'REMOVE_ALERT':
             return state.filter(alert => alert.id !== payload)
