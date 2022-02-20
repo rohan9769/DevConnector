399fdd7164463501f137204492b86afdccb0806c
Rohan Nayak <rohannayak2071@gmail.com>
2022-02-20 15:56:00 -0500

    authentication done

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import "./App.css";
+import { useEffect } from "react";
 import {BrowserRouter as Router, Route,  Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Landing from "./components/layout/Landing";
 import Alert from "./components/layout/Alert";
+import {loadUser} from './actions/auth'
+import setAuthToken from "./utils/setAuthToken";
 // Redux stuff
 import {Provider} from 'react-redux'
 import store from './store'
 
-function App() {
+if(localStorage.token){
+  setAuthToken(localStorage.token)
+}
+
+const App = () => {
+  useEffect(()=>{
+    store.dispatch(loadUser())
+  },[])
+
+
   return (
     <Provider store={store}>
       <Router>
