49d150282b30b80fb65f31d00b997196942ded72
Rohan Nayak <rohannayak2071@gmail.com>
2022-02-21 21:22:14 -0500

    created createprofile component

---
+++ client/src/components/profile-form/CreateProfile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+
+
+const CreateProfile = () =>{
+    return(
+        <>
+
+        </>
+    )
+}
+
+CreateProfile.propTypes = {
+
+}
+
+
+export default CreateProfile
